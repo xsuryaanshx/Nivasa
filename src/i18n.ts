9a92d64ca0ad5d0b4bf53737ae9566ff583ad93f
xsuryaanshx <1923.suryansh@gmail.com>
2026-05-11 02:15:12 +0530

    gii

---
+++ src/i18n.ts
@@ -0,0 +1,32 @@
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
+
+import enCommon from './locales/en/common.json';
+import hiCommon from './locales/hi/common.json';
+
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        common: enCommon,
+      },
+      hi: {
+        common: hiCommon,
+      },
+    },
+    fallbackLng: 'en',
+    ns: ['common'],
+    defaultNS: 'common',
+    interpolation: {
+      escapeValue: false,
+    },
+    detection: {
+      order: ['localStorage', 'navigator'],
+      caches: ['localStorage'],
+    },
+  });
+
+export default i18n;
