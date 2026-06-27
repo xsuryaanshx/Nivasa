a5de3f30f65b65a07996ad3f2271aa69a7dd9fa1
rohit7518 <rohittiwary54@gmail.com>
2026-06-27 19:21:14 +0530

    fix: completely disable native Android scrollbars in MainActivity

--- android/app/src/main/java/com/nivasa/app/MainActivity.java
+++ android/app/src/main/java/com/nivasa/app/MainActivity.java
@@ -2,4 +2,13 @@ package com.nivasa.app;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (bridge != null && bridge.getWebView() != null) {
+            bridge.getWebView().setVerticalScrollBarEnabled(false);
+            bridge.getWebView().setHorizontalScrollBarEnabled(false);
+        }
+    }
+}
